5a8ab5c3beb93486277a19cd55a7f1d6a7dec30a
nithi <nithindominic1924@gmail.com>
2020-12-01 23:33:32 +0530

    permission added

--- app/src/main/java/com/example/cellularsignalstrengthfinder/MainActivity.kt
+++ app/src/main/java/com/example/cellularsignalstrengthfinder/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.cellularsignalstrengthfinder
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.TelephonyManager
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+
+const val permissionCode = 200
+val permissions: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                permissions,
+                permissionCode
+        )
+    }
+
 }
